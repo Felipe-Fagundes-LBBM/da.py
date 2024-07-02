e2fd5f3fb542bce60ae427d3ea9c6e7b56839ad9
Felipe Lopes Fagundes <felipelopesfagundes@gmail.com>
2024-07-02 18:26:08 -0300

    ok

--- da.py
+++ da.py
@@ -1,1 +1,16 @@
+
+# Comentário única linha
+
+# Atribuindo valores a variaveis
+x = 5
+name = "João"
+
+# tipos de dados basicos
+inteiro = 10
+flutuante = 3.14
+booleano = True
+string = "Hello!"
+
+
+
 print("Hello!")
